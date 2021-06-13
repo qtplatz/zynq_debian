1180f45ade6f286142affc2015c3b8fa4ceaac49
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2021-06-13 16:53:29 +0900

    add cma=256m for boot.cmd

--- src/boot.cmd
+++ src/boot.cmd
@@ -3,7 +3,7 @@
 # mkimage -C none -A arm -T script -d boot.cmd boot.scr
 #env set bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootfstype=ext4 rw rootwait fsck.repair=yes panic=10
 # load load mmc 0:1 ${scriptaddr} boot.scr
-env set bootargs root=/dev/mmcblk0p2 rw
+env set bootargs root=/dev/mmcblk0p2 rw cma=256m
 fatload mmc 0 ${kernel_addr_r} zImage
 fatload mmc 0 ${fdt_addr_r} u-boot.dtb
 bootz ${kernel_addr_r} - ${fdt_addr_r}
